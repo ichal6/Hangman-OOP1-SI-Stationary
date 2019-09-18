f82af28209f55318de6b776a6655ebdd1d307c4e
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-18 15:01:33 +0200

    Write skeleton of mainMenu function

--- HANGMAN.java
+++ HANGMAN.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
 public class HANGMAN 
 {
     public static void main(String[] args) 
     {
-        HANGMAN.play_game();
+        mainMenu();
     }
 
     public static void play_game()
     {
         FILE_OPERATION.open_file("countries_and_capitals.txt");
+    }
+
+    public static int mainMenu()
+    {
+        System.out.println("Welcome to Hangman! Save the world from SKYNET!");
+        Scanner inputUser = new Scanner(System.in);  // Create a Scanner object
+        char choose_user = inputUser.next().charAt(0);  // Read user input
+        switch(choose_user)
+        {
+            case 'S':
+                System.out.println("Heya! Nacisnąłeś start!");
+                break;
+            case 'H':
+                break;
+            case 'E':
+                break;
+            default:
+                System.out.println("You press wrong input. Please try again");
+                mainMenu();
+        }
+        return 1;
     }
 }
