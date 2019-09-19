d0351565e8fdf267255356bd7667bd4d47bc6b50
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-19 18:51:35 +0200

    Repair  mainMenu
    So far by back to main menu program call recurvise mainMenu.
    Now program have boolean isRun service run program

--- HANGMAN.java
+++ HANGMAN.java
@@ -8,28 +8,32 @@ public class HANGMAN
         mainMenu();
     }
 
-    public static int mainMenu()
+    public static void mainMenu()
     {
         System.out.println("(S)tart\n(H)igh scores\n(E)xit");
         Scanner inputUser = new Scanner(System.in);  // Create a Scanner object
         char choose_user = inputUser.next().charAt(0);  // Read user input
         choose_user = Character.toUpperCase(choose_user);
-        switch(choose_user)
+        boolean isRun = true;
+        while (isRun)
         {
-            case 'S':
-                PLAY_GAME.initGame();
-                mainMenu();
-            case 'H':
-                Scanner dataFromListWin = FILE_OPERATION.open_file("win_list.txt");  
-                System.out.print(FILE_OPERATION.arrayToString(FILE_OPERATION.ScannertoArray(dataFromListWin, true)));
-                mainMenu();
-            case 'E':
-                break;
-            default:
-                System.out.println("You press wrong input. Please try again");
-                mainMenu();
+            switch(choose_user)
+            {
+                case 'S':
+                    PLAY_GAME.initGame();
+                    break;
+                case 'H':
+                    Scanner dataFromListWin = FILE_OPERATION.open_file("win_list.txt");  
+                    System.out.print(FILE_OPERATION.arrayToString(FILE_OPERATION.ScannertoArray(dataFromListWin, true)));
+                    break;
+                case 'E':
+                    isRun = false;
+                    break;
+                default:
+                    System.out.println("You press wrong input. Please try again");
+            }
         }
-        return 1;
+
     }
 
 
