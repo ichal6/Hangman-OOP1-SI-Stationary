368952f3635c6a9e720f0848d85f43181eaebbf6
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-20 11:06:51 +0200

    Refactor FIND_LETTER

--- FIND_LETTER.java
+++ FIND_LETTER.java
@@ -2,7 +2,7 @@ public class FIND_LETTER
 {
     public static void main(String[] args) 
     {
-        ;
+    
     }
 
     public static boolean checkWordInText(String capital, String letterOrWord)
